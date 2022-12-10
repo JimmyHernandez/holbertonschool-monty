95765afcda6f854f027383fb20ba3ca5df2dab4d
Enyel019 <Enyelfm@gmail.com>
2022-12-10 17:24:27 +0000

    fix update

--- monty.h
+++ monty.h
@@ -1,20 +1,11 @@
-#ifndef _MONTY_H
-#define _MONTY_H
-
-/*A global variable that is used to store the arguments passed to the program*/
-char **GlobalVar;
-
-        /*LIBRARIES*/
-        
+#ifndef _MONTY_H_
+#define _MONTY_H_
+#define _GNU_SOURCE
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <signal.h>
-#include <ctype.h>
-#include <stddef.h>
-
-        /*STRUCTURES*/
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -27,9 +18,9 @@ char **GlobalVar;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -42,18 +33,78 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/**
+ * struct globalvariables - global struct of variables
+ * @num: value of node
+ * @line: line from getline
+ * @mfile: textfile
+ * @stack: pointer to the head of stack_t
+ * @filename: file name string
+ * @token: token
+ *
+ * Description: variables to share between functions
+ */
+typedef struct globalvariables
+{
+	int num;
+	char *line;
+	FILE *mfile;
+	stack_t *stack;
+	char *filename;
+	char *token;
+} gbv;
 
-        /*PROTOTYPES*/
+extern gbv gv;
 
-void push_monty(stack_t **stack, unsigned int line_number);
-void pall_monty(stack_t **stack, unsigned int line_number);
-void pint_monty(stack_t **stack, unsigned int line_number);
-void pop_monty(stack_t **stack, unsigned int line_number);
-void swap_monty(stack_t **stack, unsigned int line_number);
-void nop_monty(stack_t **stack, unsigned int line_number);
-void add_monty(stack_t **stack, unsigned int line_number);
+typedef struct data_s
+{
+	char *line;
+	char **words;
+	stack_t *stack;
+	FILE *fptr;
+	int qflag;
+} data_t;
 
-#endif 
+typedef stack_t dlistint_t;
+
+extern data_t data;
+
+void errorHandler(unsigned int errno, unsigned int line_number);
+void errorHandler2(unsigned int errno, unsigned int line_number);
+char *isNumber(char *value, unsigned int line_number);
+void compare(char *token, stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub_handler(stack_t **stack, unsigned int line_number);
+void div_handler(stack_t **stack, unsigned int line_number);
+void mul_handler(stack_t **stack, unsigned int line_number);
+void mod_handler(stack_t **stack, unsigned int line_number);
+
+void freeAll(void);
+void freeLinkedList(stack_t *stack);
+void free_all(int all);
+int count_word(char *s);
+char **strtow(char *str);
+
+
+/* list_funcs1.c */
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+size_t print_dlistint(const dlistint_t *h);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
+/* list_funcs2.c */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+
+#endif
