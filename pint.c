f57e8ee9b897e80cc04ec7a50a96062a1fe62ddb
Enyel019 <Enyelfm@gmail.com>
2022-12-10 17:43:15 +0000

    1.pint

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+* pint - print the first int
+* @stack: pointer to pointer
+* @line_number: unsigned int
+*/
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (!*stack)
+		errorHandler(5, line_number);
+	printf("%d\n", temp->n);
+
+}
